b9649e84ec3318a1f11c5811da5f67ed53db145f
Ben Vaughan <56156727+ben-vaughan@users.noreply.github.com>
2023-01-31 18:42:58 +0000

    Initial project setup

---
+++ front-end/src/index.js
@@ -0,0 +1,16 @@
+// Libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// User-defined components
+import App from './App';
+
+// Styling
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
